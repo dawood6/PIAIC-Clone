066659434b44165ec615733ce7d0f59c61124e36
dawood6 <dawoodsadiq171@gmail.com>
2020-05-10 22:19:55 +0500

    feat: new changes

---
+++ app.js
@@ -0,0 +1,14 @@
+// (function () {
+//   var head = $(".head1");
+//   var head1Index = -1;
+
+//   function showHead1() {
+//     ++head1Index;
+//     head.eq(head1Index % head.length)
+//       .fadeIn(2000)
+//       .delay(2000);
+//   }
+
+//   showHead1();
+// })();
+
