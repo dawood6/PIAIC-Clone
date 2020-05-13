26908c578781575602ef05f88080e8605b217e5d
dawood6 <dawoodsadiq171@gmail.com>
2020-05-13 23:53:56 +0500

    feat: updated code

--- app.js
+++ app.js
@@ -2,3 +2,116 @@ const k = document.getElementById('odometer-k')
 setTimeout(function(){
     k.innerHTML = 'K+';
 }, 2800)
+
+function video1() {
+    document.getElementById("videoHeading1").style.textDecoration = "underline";
+    document.getElementById("videoHeading1").style.color = "rgb(9 , 187 , 140)" 
+    document.getElementById("videoHeading3").style.textDecoration = "none";
+    document.getElementById("videoHeading3").style.color = "rgb(141, 209, 191)" 
+    document.getElementById("videoHeading2").style.textDecoration = "none";
+    document.getElementById("videoHeading2").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading4").style.textDecoration = "none";
+    document.getElementById("videoHeading4").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading5").style.textDecoration = "none";
+    document.getElementById("videoHeading5").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading6").style.textDecoration = "none";
+    document.getElementById("videoHeading6").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading7").style.textDecoration = "none";
+    document.getElementById("videoHeading7").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading8").style.textDecoration = "none";
+    document.getElementById("videoHeading8").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading9").style.textDecoration = "none";
+    document.getElementById("videoHeading9").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading10").style.textDecoration = "none";
+    document.getElementById("videoHeading10").style.color = "rgb(141, 209, 191)"
+  }
+  function video2() {
+    document.getElementById("videoHeading3").style.textDecoration = "underline";
+    document.getElementById("videoHeading3").style.color = "rgb(9 , 187 , 140)" 
+    document.getElementById("videoHeading1").style.textDecoration = "none";
+    document.getElementById("videoHeading1").style.color = "rgb(141, 209, 191)" 
+    document.getElementById("videoHeading2").style.textDecoration = "none";
+    document.getElementById("videoHeading2").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading4").style.textDecoration = "none";
+    document.getElementById("videoHeading4").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading5").style.textDecoration = "none";
+    document.getElementById("videoHeading5").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading6").style.textDecoration = "none";
+    document.getElementById("videoHeading6").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading7").style.textDecoration = "none";
+    document.getElementById("videoHeading7").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading8").style.textDecoration = "none";
+    document.getElementById("videoHeading8").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading9").style.textDecoration = "none";
+    document.getElementById("videoHeading9").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading10").style.textDecoration = "none";
+    document.getElementById("videoHeading10").style.color = "rgb(141, 209, 191)"
+  }
+  function video3() {
+    document.getElementById("videoHeading3").style.textDecoration = "underline";
+    document.getElementById("videoHeading3").style.color = "rgb(9 , 187 , 140)" 
+    document.getElementById("videoHeading1").style.textDecoration = "none";
+    document.getElementById("videoHeading1").style.color = "rgb(141, 209, 191)" 
+    document.getElementById("videoHeading2").style.textDecoration = "none";
+    document.getElementById("videoHeading2").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading4").style.textDecoration = "none";
+    document.getElementById("videoHeading4").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading5").style.textDecoration = "none";
+    document.getElementById("videoHeading5").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading6").style.textDecoration = "none";
+    document.getElementById("videoHeading6").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading7").style.textDecoration = "none";
+    document.getElementById("videoHeading7").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading8").style.textDecoration = "none";
+    document.getElementById("videoHeading8").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading9").style.textDecoration = "none";
+    document.getElementById("videoHeading9").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading10").style.textDecoration = "none";
+    document.getElementById("videoHeading10").style.color = "rgb(141, 209, 191)"
+  }
+  function video4() {
+    document.getElementById("videoHeading4").style.textDecoration = "underline";
+    document.getElementById("videoHeading4").style.color = "rgb(9 , 187 , 140)" 
+    document.getElementById("videoHeading1").style.textDecoration = "none";
+    document.getElementById("videoHeading1").style.color = "rgb(141, 209, 191)" 
+    document.getElementById("videoHeading2").style.textDecoration = "none";
+    document.getElementById("videoHeading2").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading3").style.textDecoration = "none";
+    document.getElementById("videoHeading3").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading5").style.textDecoration = "none";
+    document.getElementById("videoHeading5").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading6").style.textDecoration = "none";
+    document.getElementById("videoHeading6").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading7").style.textDecoration = "none";
+    document.getElementById("videoHeading7").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading8").style.textDecoration = "none";
+    document.getElementById("videoHeading8").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading9").style.textDecoration = "none";
+    document.getElementById("videoHeading9").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading10").style.textDecoration = "none";
+    document.getElementById("videoHeading10").style.color = "rgb(141, 209, 191)"
+  }
+  function video5() {
+    document.getElementById("videoHeading5").style.textDecoration = "underline";
+    document.getElementById("videoHeading5").style.color = "rgb(9 , 187 , 140)" 
+  }
+  function video6() {
+    document.getElementById("videoHeading6").style.textDecoration = "underline";
+    document.getElementById("videoHeading6").style.color = "rgb(9 , 187 , 140)" 
+  }
+  function video7() {
+    document.getElementById("videoHeading7").style.textDecoration = "underline";
+    document.getElementById("videoHeading7").style.color = "rgb(9 , 187 , 140)" 
+  }
+  function video8() {
+    document.getElementById("videoHeading8").style.textDecoration = "underline";
+    document.getElementById("videoHeading8").style.color = "rgb(9 , 187 , 140)" 
+  }
+  function video9() {
+    document.getElementById("videoHeading9").style.textDecoration = "underline";
+    document.getElementById("videoHeading9").style.color = "rgb(9 , 187 , 140)" 
+  }
+  function video10() {
+    document.getElementById("videoHeading10").style.textDecoration = "underline";
+    document.getElementById("videoHeading10").style.color = "rgb(9 , 187 , 140)" 
+  }
