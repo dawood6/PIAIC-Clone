7d19a4b7e21a5e1570e28c75c263c3f49caa2e23
dawood6 <dawoodsadiq171@gmail.com>
2020-05-14 01:02:32 +0500

    feat: updated code

--- app.js
+++ app.js
@@ -24,14 +24,17 @@ function video1() {
     document.getElementById("videoHeading9").style.color = "rgb(141, 209, 191)"
     document.getElementById("videoHeading10").style.textDecoration = "none";
     document.getElementById("videoHeading10").style.color = "rgb(141, 209, 191)"
-  }
+    document.getElementById('youtubeVideos').innerHTML = "<iframe width='817' height='480' src='https://www.youtube.com/embed/m4g0zd4E_bo?autoplay=1' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+    document.getElementById("youHead").innerHTML = "PIAIC Offering Free First Quarter Classes Online"  
+    document.getElementById("youPar").innerHTML = "The mission of PIAIC is to reshape Pakistan by revolutionizing education, research, and <br /> business by adopting latest, cutting-edge technologies."
+}
   function video2() {
-    document.getElementById("videoHeading3").style.textDecoration = "underline";
-    document.getElementById("videoHeading3").style.color = "rgb(9 , 187 , 140)" 
+    document.getElementById("videoHeading2").style.textDecoration = "underline";
+    document.getElementById("videoHeading2").style.color = "rgb(9 , 187 , 140)" 
     document.getElementById("videoHeading1").style.textDecoration = "none";
     document.getElementById("videoHeading1").style.color = "rgb(141, 209, 191)" 
-    document.getElementById("videoHeading2").style.textDecoration = "none";
-    document.getElementById("videoHeading2").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading3").style.textDecoration = "none";
+    document.getElementById("videoHeading3").style.color = "rgb(141, 209, 191)"
     document.getElementById("videoHeading4").style.textDecoration = "none";
     document.getElementById("videoHeading4").style.color = "rgb(141, 209, 191)"
     document.getElementById("videoHeading5").style.textDecoration = "none";
@@ -46,7 +49,10 @@ function video1() {
     document.getElementById("videoHeading9").style.color = "rgb(141, 209, 191)"
     document.getElementById("videoHeading10").style.textDecoration = "none";
     document.getElementById("videoHeading10").style.color = "rgb(141, 209, 191)"
-  }
+    document.getElementById('youtubeVideos').innerHTML = "<iframe width='817' height='480' src='https://www.youtube.com/embed/NPGapK5aMII?autoplay=1' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+    document.getElementById("youHead").innerHTML = "PIAIC OFFICIAL COVID-19 Coronavirus Policy"
+    document.getElementById("youPar").innerHTML = "The mission of PIAIC is to reshape Pakistan by revolutionizing education, research, and <br /> business by adopting latest, cutting-edge technologies."
+}
   function video3() {
     document.getElementById("videoHeading3").style.textDecoration = "underline";
     document.getElementById("videoHeading3").style.color = "rgb(9 , 187 , 140)" 
@@ -68,7 +74,10 @@ function video1() {
     document.getElementById("videoHeading9").style.color = "rgb(141, 209, 191)"
     document.getElementById("videoHeading10").style.textDecoration = "none";
     document.getElementById("videoHeading10").style.color = "rgb(141, 209, 191)"
-  }
+    document.getElementById('youtubeVideos').innerHTML = "<iframe width='817' height='480' src='https://www.youtube.com/embed/_rUQ301EJAo?autoplay=1' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+    document.getElementById("youHead").innerHTML = "PIAIC Classes Coming Soon!"
+    document.getElementById("youPar").innerHTML = "The mission of PIAIC is to reshape Pakistan by revolutionizing education, research, and <br /> business by adopting latest, cutting-edge technologies."
+}
   function video4() {
     document.getElementById("videoHeading4").style.textDecoration = "underline";
     document.getElementById("videoHeading4").style.color = "rgb(9 , 187 , 140)" 
@@ -90,28 +99,157 @@ function video1() {
     document.getElementById("videoHeading9").style.color = "rgb(141, 209, 191)"
     document.getElementById("videoHeading10").style.textDecoration = "none";
     document.getElementById("videoHeading10").style.color = "rgb(141, 209, 191)"
-  }
+    document.getElementById('youtubeVideos').innerHTML = "<iframe width='817' height='480' src='https://www.youtube.com/embed/RuYbBHHF4VM?autoplay=1' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+    document.getElementById("youHead").innerHTML = "Teaser 2: PIAIC Classes Coming Soon"
+    document.getElementById("youPar").innerHTML = "The mission of PIAIC is to reshape Pakistan by revolutionizing education, research, and <br /> business by adopting latest, cutting-edge technologies."
+}
   function video5() {
     document.getElementById("videoHeading5").style.textDecoration = "underline";
     document.getElementById("videoHeading5").style.color = "rgb(9 , 187 , 140)" 
-  }
+    document.getElementById("videoHeading1").style.textDecoration = "none";
+    document.getElementById("videoHeading1").style.color = "rgb(141, 209, 191)" 
+    document.getElementById("videoHeading2").style.textDecoration = "none";
+    document.getElementById("videoHeading2").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading4").style.textDecoration = "none";
+    document.getElementById("videoHeading4").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading3").style.textDecoration = "none";
+    document.getElementById("videoHeading3").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading6").style.textDecoration = "none";
+    document.getElementById("videoHeading6").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading7").style.textDecoration = "none";
+    document.getElementById("videoHeading7").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading8").style.textDecoration = "none";
+    document.getElementById("videoHeading8").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading9").style.textDecoration = "none";
+    document.getElementById("videoHeading9").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading10").style.textDecoration = "none";
+    document.getElementById("videoHeading10").style.color = "rgb(141, 209, 191)"
+    document.getElementById('youtubeVideos').innerHTML = "<iframe width='817' height='480' src='https://www.youtube.com/embed/sqtc7s1KZEg?autoplay=1' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+    document.getElementById("youHead").innerHTML = "PIAIC Intro - Full Version"
+    document.getElementById("youPar").innerHTML = "Official intro video of the PIAIC. Messages from President Dr. Arif Alvi, Kazi Rahat Ali, Hunaid <br /> Lakhani, Sulaiman S. Mehdi, and Zia Ullah Khan."
+}
   function video6() {
     document.getElementById("videoHeading6").style.textDecoration = "underline";
     document.getElementById("videoHeading6").style.color = "rgb(9 , 187 , 140)" 
-  }
+    document.getElementById("videoHeading1").style.textDecoration = "none";
+    document.getElementById("videoHeading1").style.color = "rgb(141, 209, 191)" 
+    document.getElementById("videoHeading2").style.textDecoration = "none";
+    document.getElementById("videoHeading2").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading4").style.textDecoration = "none";
+    document.getElementById("videoHeading4").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading5").style.textDecoration = "none";
+    document.getElementById("videoHeading5").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading3").style.textDecoration = "none";
+    document.getElementById("videoHeading3").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading7").style.textDecoration = "none";
+    document.getElementById("videoHeading7").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading8").style.textDecoration = "none";
+    document.getElementById("videoHeading8").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading9").style.textDecoration = "none";
+    document.getElementById("videoHeading9").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading10").style.textDecoration = "none";
+    document.getElementById("videoHeading10").style.color = "rgb(141, 209, 191)"
+    document.getElementById('youtubeVideos').innerHTML = "<iframe width='817' height='480' src='https://www.youtube.com/embed/kHLdqsLWnEMautoplay=1' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+    document.getElementById("youHead").innerHTML = "PTV Coverage of PIAIC Entry Test"
+    document.getElementById("youPar").innerHTML = "PTV News coverage of 1st Entrance Exam of the PIAIC in Karachi"
+}
   function video7() {
     document.getElementById("videoHeading7").style.textDecoration = "underline";
     document.getElementById("videoHeading7").style.color = "rgb(9 , 187 , 140)" 
-  }
+    document.getElementById("videoHeading1").style.textDecoration = "none";
+    document.getElementById("videoHeading1").style.color = "rgb(141, 209, 191)" 
+    document.getElementById("videoHeading2").style.textDecoration = "none";
+    document.getElementById("videoHeading2").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading4").style.textDecoration = "none";
+    document.getElementById("videoHeading4").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading5").style.textDecoration = "none";
+    document.getElementById("videoHeading5").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading6").style.textDecoration = "none";
+    document.getElementById("videoHeading6").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading3").style.textDecoration = "none";
+    document.getElementById("videoHeading3").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading8").style.textDecoration = "none";
+    document.getElementById("videoHeading8").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading9").style.textDecoration = "none";
+    document.getElementById("videoHeading9").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading10").style.textDecoration = "none";
+    document.getElementById("videoHeading10").style.color = "rgb(141, 209, 191)"
+    document.getElementById('youtubeVideos').innerHTML = "<iframe width='817' height='80' src='https://www.youtube.com/embed/7fjcWkDFbcg?autoplay=1' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+    document.getElementById("youHead").innerHTML = "PIAIC Launch - President Arif Alvi Address"
+    document.getElementById("youPar").innerHTML = "Presidential address at the launch of the Presidential Initiative for Artificial Intelligence and <br /> Computing on December 9, 2018."
+}
   function video8() {
     document.getElementById("videoHeading8").style.textDecoration = "underline";
-    document.getElementById("videoHeading8").style.color = "rgb(9 , 187 , 140)" 
-  }
+    document.getElementById("videoHeading8").style.color = "rgb(9 , 187 , 140)"
+    document.getElementById("videoHeading1").style.textDecoration = "none";
+    document.getElementById("videoHeading1").style.color = "rgb(141, 209, 191)" 
+    document.getElementById("videoHeading2").style.textDecoration = "none";
+    document.getElementById("videoHeading2").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading4").style.textDecoration = "none";
+    document.getElementById("videoHeading4").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading5").style.textDecoration = "none";
+    document.getElementById("videoHeading5").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading6").style.textDecoration = "none";
+    document.getElementById("videoHeading6").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading7").style.textDecoration = "none";
+    document.getElementById("videoHeading7").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading3").style.textDecoration = "none";
+    document.getElementById("videoHeading3").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading9").style.textDecoration = "none";
+    document.getElementById("videoHeading9").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading10").style.textDecoration = "none";
+    document.getElementById("videoHeading10").style.color = "rgb(141, 209, 191)" 
+    document.getElementById('youtubeVideos').innerHTML = "<iframe width='817' height='480' src='https://www.youtube.com/embed/C5iWrmShgv0?autoplay=1' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+    document.getElementById("youHead").innerHTML = "Zia Khan Interview on Bol News"
+    document.getElementById("youPar").innerHTML = "Published on Dec 31, 2018"
+}
   function video9() {
     document.getElementById("videoHeading9").style.textDecoration = "underline";
     document.getElementById("videoHeading9").style.color = "rgb(9 , 187 , 140)" 
-  }
+    document.getElementById("videoHeading1").style.textDecoration = "none";
+    document.getElementById("videoHeading1").style.color = "rgb(141, 209, 191)" 
+    document.getElementById("videoHeading2").style.textDecoration = "none";
+    document.getElementById("videoHeading2").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading4").style.textDecoration = "none";
+    document.getElementById("videoHeading4").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading5").style.textDecoration = "none";
+    document.getElementById("videoHeading5").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading6").style.textDecoration = "none";
+    document.getElementById("videoHeading6").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading7").style.textDecoration = "none";
+    document.getElementById("videoHeading7").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading8").style.textDecoration = "none";
+    document.getElementById("videoHeading8").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading3").style.textDecoration = "none";
+    document.getElementById("videoHeading3").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading10").style.textDecoration = "none";
+    document.getElementById("videoHeading10").style.color = "rgb(141, 209, 191)"
+    document.getElementById('youtubeVideos').innerHTML = "<iframe width='817' height='480' src='https://www.youtube.com/embed/5p8oJVVg2P4?autoplay=1' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+    document.getElementById("youHead").innerHTML = "PTV Coverage of PIAIC Launch Event"
+    document.getElementById("youPar").innerHTML = "PTV Coverage of PIAIC Launch Event on December 9, 2018"
+}
   function video10() {
     document.getElementById("videoHeading10").style.textDecoration = "underline";
-    document.getElementById("videoHeading10").style.color = "rgb(9 , 187 , 140)" 
-  }
+    document.getElementById("videoHeading10").style.color = "rgb(9 , 187 , 140)"
+    document.getElementById("videoHeading1").style.textDecoration = "none";
+    document.getElementById("videoHeading1").style.color = "rgb(141, 209, 191)" 
+    document.getElementById("videoHeading2").style.textDecoration = "none";
+    document.getElementById("videoHeading2").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading4").style.textDecoration = "none";
+    document.getElementById("videoHeading4").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading5").style.textDecoration = "none";
+    document.getElementById("videoHeading5").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading6").style.textDecoration = "none";
+    document.getElementById("videoHeading6").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading7").style.textDecoration = "none";
+    document.getElementById("videoHeading7").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading8").style.textDecoration = "none";
+    document.getElementById("videoHeading8").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading9").style.textDecoration = "none";
+    document.getElementById("videoHeading9").style.color = "rgb(141, 209, 191)"
+    document.getElementById("videoHeading3").style.textDecoration = "none";
+    document.getElementById("videoHeading3").style.color = "rgb(141, 209, 191)" 
+    document.getElementById('youtubeVideos').innerHTML = "<iframe width='817' height='480' src='https://www.youtube.com/embed/dRm0Y7n7xzU?autoplay=1' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+    document.getElementById("youHead").innerHTML = "LIVE: President of Pakistan Dr Arif Alvi's exclusive interview with Nadeem Malik, October <br/> 29, 2018"
+    document.getElementById("youPar").innerHTML = "Streamed live on Oct 29, 2018"
+}
